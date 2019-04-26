db740b85b0a96a6f54c7533e3f9034f2865a4480
Ryan <1240363663@qq.com>
2019-04-26 17:42:13 +0800

    .

--- src/com/leetcodeC/primaryalgorithms/MaxDepOfBinTree.java
+++ src/com/leetcodeC/primaryalgorithms/MaxDepOfBinTree.java
@@ -36,4 +36,16 @@ public class MaxDepOfBinTree {
         }
         return depth;
     }
+
+    // better implementation
+
+    /*
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        }
+    }
+    */
 }
